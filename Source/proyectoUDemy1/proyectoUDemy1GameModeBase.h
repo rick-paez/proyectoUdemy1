651116da79abc4a1347c2260b53c9deab6132744
Rick <deathwarsoldier@gmail.com>
2018-05-15 22:45:41 -0500

    Initial commit
    Primer commit

---
+++ Source/proyectoUDemy1/proyectoUDemy1GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "proyectoUDemy1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROYECTOUDEMY1_API AproyectoUDemy1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
